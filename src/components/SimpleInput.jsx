09ecfd153c7d76b98caca8f175dcfb54681c2879
falamarcao <falamarcao@gmail.com>
2022-12-29 19:32:42 -0300

    202. Handling the "was touched" State

--- src/components/SimpleInput.jsx
+++ src/components/SimpleInput.jsx
@@ -1,20 +1,37 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 // This is an example, the of useState and/or useRef depends on the objective.
 const SimpleInput = (props) => {
-  const [state, setState] = useState("");
   const nameRef = useRef();
+  const [state, setState] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  const [isTouched, setIsTouched] = useState(false);
+
+  const isInvalidAndTouched = !isValid && isTouched;
+
+  useEffect(() => {
+    if (isValid) {
+      console.log("Field  " + nameRef.current.id + " is valid!");
+    }
+  }, [isValid]);
 
   const handleOnChange = (event) => {
+    if (!isTouched) {
+      setIsTouched(true);
+    }
     setState(event.target.value);
   };
 
   const handleOnSubmit = (event) => {
     event.preventDefault();
 
+    setIsTouched(true);
+
     if (state.trim() === "") {
+      setIsValid(false);
       return;
     }
+    setIsValid(true);
 
     console.log("useState:\n" + state);
 
@@ -27,7 +44,7 @@ const SimpleInput = (props) => {
 
   return (
     <form>
-      <div className="form-control">
+      <div className={"form-control" + (isInvalidAndTouched ? " invalid" : "")}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameRef}
@@ -36,6 +53,9 @@ const SimpleInput = (props) => {
           onChange={handleOnChange}
           value={state}
         />
+        {isInvalidAndTouched && (
+          <p className="error-text">You must provide Your Name.</p>
+        )}
       </div>
       <div className="form-actions">
         <button onClick={handleOnSubmit}>Submit</button>

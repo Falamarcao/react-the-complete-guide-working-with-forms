f328a91753885c2cee8f07771111ad918200817c
falamarcao <falamarcao@gmail.com>
2022-12-29 18:14:51 -0300

    199. Dealing With Form Submission & Getting User Input Values

--- src/components/SimpleInput.jsx
+++ src/components/SimpleInput.jsx
@@ -1,12 +1,40 @@
+import { useRef, useState } from "react";
+
+// This is an example, the of useState and/or useRef depends on the objective.
 const SimpleInput = (props) => {
+  const [state, setState] = useState("");
+  const nameRef = useRef();
+
+  const handleOnChange = (event) => {
+    setState(event.target.value);
+  };
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+
+    console.log("useState:\n" + state);
+
+    console.log("useRef:\n" + nameRef.current.value);
+
+    setState("");
+
+    // nameRef.current.value = ""; // NOT A BEST PRACTICE to manipulate directly the DOM.
+  };
+
   return (
     <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameRef}
+          type="text"
+          id="name"
+          onChange={handleOnChange}
+          value={state}
+        />
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button onClick={handleOnSubmit}>Submit</button>
       </div>
     </form>
   );
